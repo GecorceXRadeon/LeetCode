5a09f9f02001f44aeb6b2dbe1a9b9b83ae549da4
pzxwhc <wuhuachuan712@163.com>
2016-02-27 13:22:46 +0800

    重新分析292_Nim_Game

--- 292_Nim_Game/Solution.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public class Solution {
-    public boolean canWinNim(int n) {
-        if( n == 1 || n == 2 || n == 3) return true;
-        return n % 4 == 0 ? false : true;
-    }
-}	
-
